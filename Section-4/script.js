a0d454f59680d30f8e015b743a6c62388035669c
Avi Shukla <avishuklacode@gmail.com>
2024-12-16 13:34:58 +0530

    Update script.js

--- Section-4/script.js
+++ Section-4/script.js
@@ -331,3 +331,159 @@ rest2.owner &&= '<Anonymos>';
 console.log(rest1);
 console.log(rest2);
 */
+//lecture 8
+/*
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+}
+// console.log([...menu.entries()]);
+*/
+
+//lecture 9
+/*
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+//With optional chain
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+//ex
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for(const day of days){
+  // console.log(day);
+  restaurant.openingHours[day]?.open ?? 'closed'
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+
+console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
+
+console.log(restaurant.);
+*/
+
+//lecture 9
+/*
+//Properties name
+const properties = Object.keys(openingHours);
+console.log(properties);
+let openStr = `We are open on ${properties.length} days:`;
+for (const day of Object.keys(openingHours)) {
+  openStr += `${day} ,`;
+}
+console.log(openStr);
+
+//Properties values
+
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire object
+const entries = Object.entries(openingHours);
+// console.log(entries);
+
+for (const [key,{open,close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+*/
+
+// lecture 10
+/*
+const italianFoods = new Set([
+  'pasta',
+  'gnocchi',
+  'tomatoes',
+  'olive oil',
+  'garlic',
+  'basil',
+]);
+
+const mexicanFoods = new Set([
+  'tortillas',
+  'beans',
+  'rice',
+  'tomatoes',
+  'avocado',
+  'garlic',
+]);
+
+const commonFoods = italianFoods.intersection(mexicanFoods);
+
+console.log('Intersection:', commonFoods);
+
+console.log([...commonFoods]);
+
+const italianMexicanFusion = italianFoods.union(mexicanFoods);
+console.log(italianMexicanFusion);
+console.log([...new Set([...italianFoods, ...mexicanFoods])]);
+
+const uniqueItalianFoods = italianFoods.difference(mexicanFoods);
+console.log(uniqueItalianFoods);
+
+const uniqueMexicanFoods = mexicanFoods.difference(italianFoods);
+console.log(uniqueMexicanFoods);
+
+const uniqueItalianAndMexicanFoods =
+  italianFoods.symmetricDifference(mexicanFoods);
+console.log(uniqueItalianAndMexicanFoods);
+
+console.log(italianFoods.isDisjointFrom(mexicanFoods));
+*/
+
+//Lecture 11
+
+// const rest = new Map();
+// rest.set('name', 'Mahesh');
+// rest.set(1, 'Firenze,Italy');
+// console.log(rest.set(2, 'Sitapur,India'));
+
+// rest
+//   .set('Categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are close');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// console.log(rest);
+
+// console.log(rest.size);
+
+// const question = new Map([
+//   ['question', 'What is th best programming languag ein the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'Javascript'],
+//   ['correct', 3],
+//   [true, 'Correct'],
+//   [false, 'Try Again'],
+// ]);
+// console.log(question);
+
+// //Convert object to maps
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// //quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+// }
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
+
+
