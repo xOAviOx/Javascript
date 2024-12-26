e3bf46fe0009df8f0dbb2eb6ad555a7d83dfca16
Avi Shukla <avishuklacode@gmail.com>
2024-12-27 00:53:42 +0530

    Update script.js

--- Section 5/script.js
+++ Section 5/script.js
@@ -1,238 +1,528 @@
 'use strict';
-/*
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  // numPassengers = numPassengers || 1
-  // price = price || 199
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+/////////////////////////////////////////////////
+// Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2024-12-23T17:01:17.194Z',
+    '2024-12-24T23:36:17.929Z',
+    '2024-12-25T10:51:36.790Z',
+  ],
+  currency: 'INR',
+  locale: 'en-IN', // de-DE
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 300);
-createBooking('LH123', 5);
-createBooking('LH123', undefined,5);
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 
-*/
-/*
-const flight = 'LH234';
-const avi = {
-  name: 'Avi Shukla',
-  passport: 232454432,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
-const checkIn = function (flightNum, passenger) {
-  flightNum = `LH999`;
-  passenger.name = `MR. +` + passenger.name;
 
-  if (passenger.passport === 232454432) {
-    alert('Check In!');
-  } else {
-    alert('Wrong passport');
+const accounts = [account1, account2];
+
+/////////////////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+// Functions
+const formatMovementDate = function (date, locale) {
+  const calcdaysPassed = (date1, date2) =>
+    Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
+
+  const daysPassed = calcdaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    // const day = `${date.getDate()}`.padStart(2, 0);
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    // const year = date.getFullYear();
+    // return `${day}/${month}/${year}`;
+    return new Intl.DateTimeFormat(locale).format(date);
   }
 };
-checkIn(flight, avi);
-console.log(flight);
-console.log(avi);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 100000000);
-};
-newPassport(avi);
-checkIn(flight, avi);
-*/
-/*
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
 };
 
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
+const displayMovements = function (acc, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const combinedMovsDates = acc.movements.map((mov, i) => {
+    return { movement: mov, movementDate: acc.movementsDates.at(i) };
+  });
+
+  if (sort) combinedMovsDates.sort((a, b) => a.movement - b.movement);
+
+  combinedMovsDates.forEach(function (obj, i) {
+    const { movement, movementDate } = obj;
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+    const date = new Date(movementDate);
+    const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formatCur(movement, acc.locale, acc.currency);
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__date">${displayDate}</div>
+
+        <div class="movements__value">${formattedMov}</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
 };
 
-//Higher order function
-const transformer = function (str, fn) {
-  console.log(`Original String: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-  console.log(`Transformed by: ${fn.name}`);
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
-transformer('JavaScript is the best!', upperFirstWord);
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
-transformer('JavaScript is the best!', oneWord);
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
-//JS uses callbacks all the time
-const high5 = function () {
-  console.log('Hii');
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = formatCur(
+    interest.toFixed(2),
+    acc.locale,
+    acc.currency
+  );
 };
-document.body.addEventListener('click', high5);
-['Avi', 'Martha', 'Adam'].forEach(high5);
-*/
-/*
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+const startLogoutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    //in each cal, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`;
+
+    //When 0 Seconds, stop timer and log out user
+    if (time === 0) {
+      clearInterval(timer);
+      labelWelcome.textContent = 'Log in to get started.';
+      containerApp.style.opacity = 0;
+    }
+    //decrease 1 second
+    time--;
   };
+
+  // Set time to 5 minutes
+  let time = 120;
+
+  //call the timer every second
+  tick();
+  const timer = setInterval(tick, 1000);
+  return timer;
 };
-const greeterHey = greet('Hey');
 
-greeterHey('Avi');
-greeterHey('Steven');
+///////////////////////////////////////
+// Event handlers
+let currentAccount, timer;
+//Fake Login
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
 
-greet('Hello')('Avi');
+//day/month/year
 
-const greet1 = greeting1 => name1 => console.log(`${greeting1} ${name1}`);
+//Experimenting API
 
-greet1('Yo')('Avi');
-*/
-/*
-const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //Create current date and time
+    const now = new Date();
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      year: 'numeric',
+    };
+    // const locale = navigator.language;
+    // console.log(locale);
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+    // const day = `${now.getDate()}`.padStart(2, 0);
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    // const year = now.getFullYear();
+    // const hour = `${now.getHours()}`.padStart(2, 0);
+    // const min = `${now.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    //timer
+    if (timer) clearInterval(timer);
+
+    timer = startLogoutTimer();
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = +inputTransferAmount.value;
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    //Add transfer dates
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
+    // Update UI
+    updateUI(currentAccount);
+
+    //reset timer
+    clearInterval(timer);
+    timer = startLogoutTimer();
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Math.floor(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    setTimeout(function () {
+      // Add movement
+      currentAccount.movements.push(amount);
+
+      //Add Loan dates
+      currentAccount.movementsDates.push(new Date().toISOString());
+
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
+  }
+  inputLoanAmount.value = '';
+  //reset timer
+  clearInterval(timer);
+  timer = startLogoutTimer();
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    +inputClosePin.value === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
     );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
+    console.log(index);
+    // .indexOf(23)
 
-lufthansa.book(239, 'Avi Shukla');
-lufthansa.book(453, 'John Smith');
-console.log(lufthansa);
+    // Delete account
+    accounts.splice(index, 1);
 
-const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
 
-const book = lufthansa.book;
-// book(23, 'Sarah WIlliam'); //Doesnt wokrk
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 
-//Call Method
-book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount, !sorted);
+  sorted = !sorted;
+});
 
-book.call(lufthansa, 239, 'Mary cooper');
-console.log(lufthansa);
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
 
-const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
-  bookings: [],
-};
-book.call(swiss, 583, 'Mary Cooper');
+// console.log(23 === 23.0);
 
-//Apply Method
+// //Base 10 - 0 to 9
+// //Binary base 2 - 0 1
+// console.log(0.1 + 0.1);
+// console.log(Number('23'));
 
-const flightData = [583, 'George Cooper'];
-book.apply(swiss, flightData);
-console.log(swiss);
+// //Conversion
+// console.log(+'23');
 
-book.call(swiss, ...flightData);
+// //Parsing
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseFloat('2.5rem'));
 
-//Bind method
+// // check if value is a nan
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20'));
+// console.log(Number.isNaN(23 / 0));
 
-// book.call(eurowings, 23, 'Sarah Williams');
+// // Checking if value is number
+// console.log(Number.isFinite(20));
 
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-bookEW(23, 'Steven Williams');
+// console.log(Number.isFinite(+'21X'));
 
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23('Avi Shukla');
-bookEW23('Martha Cooper');
+// console.log(Math.sqrt(25));
+// console.log(8 * (1 / 2));
+// console.log(8 * (1 / 3));
 
-// With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-  console.log(this);
-  this.planes++;
-  console.log(this.planes);
-};
-// lufthansa.buyPlane();
-document
-  .querySelector('.buy')
-  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.max(5, 18, 23, 11, 2));
+// console.log(Math.min(5, 18, 23, 11, 2));
 
-//Partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
 
-const addVAT = addTax.bind(null, 23);
- */
+// // console.log(Math.trunc(Math.random() * 6) + 1);
 
-// const runOnce = function () {
-//   console.log('this will never run again');
+// const randomInt = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
 // };
-// runOnce();
+// console.log(randomInt(10, 20));
+// console.log(randomInt(0, 4));
 
-// //IIFE
-// (function () {
-//   console.log('this will never run again');
-// })();
+//Rounding
 
-// (() => console.log('this will never run again'))();
+// console.log(Math.round(23.9));
+// console.log(Math.ceil(23.9));
+// console.log(Math.round(23.9));
+// console.log(Math.floor(23.9));
+// console.log(Math.trunc(23.3));
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
 
-// {
-//   var isPrivate = 23;
-// }
-// console.log(isPrivate);
+// //Rounding decimals
+// console.log((2.7).toFixed(0));
+
+// console.log(5 % 2);
+// console.log(8 % 3);
+// console.log(8/3);
+
+// const diameter = 2_320_000_000_000_000;
+// console.log(diameter);
+
+// const priceCents = 345_99;
+// console.log(priceCents);
+
+// const transferFee = 15_00;
+// const transferFee2 = 1_500;
+
+// const PI = 3.14_15;
+// console.log(PI);
+
+// console.log(1000000n + 10000n);
+
+// const huge = 19328273782838283288228992n;
+// const num = 23;
+
+// //Exception
+// console.log(huge * BigInt(num));
+// console.log(20n > 15);
+// console.log(20n === 20);
+// console.log(typeof 20n);
+
+// console.log(20n == 20);
+
+// console.log(huge + 'is really big');
+
+// //Divisions
+// console.log(11n/ 3n);
+
+//Create a date
 /*
-const secureBooking = function () {
-  let passengerCount = 0;
+const now = new Date();
+console.log(now);
 
-  return function () {
-    passengerCount++;
-    console.log(passengerCount + ' Passengers');
-  };
-};
-const booker = secureBooking();
-booker();
-booker();
-booker();
+console.log(new Date(account1.movementsDates[0]));
 
-console.dir(booker)
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
 */
 
-// let f;
-// const g = function () {
-//   const a = 23;
-//   f = function () {
-//     console.log(a * 2);
-//   };
+// //Working with dates
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDay());
+// console.log(future.getDate());
+// console.log(future.getHours());
+// console.log(future.getMilliseconds());
+// console.log(future.getSeconds());
+
+// console.log(future.toISOString());
+// console.log(future.getTime());
+
+// console.log(new Date(23273232));
+
+// const future = new Date(2037, 10, 19, 15, 23);
+// console.log(Number(future));
+
+// const calcdaysPassed = (date1, date2) =>
+//   Math.abs((date2 - date1) / (1000 * 60 * 60 * 24));
+// const days = calcdaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 4));
+// console.log(days);
+
+// const num = 23234332.32;
+
+// const options = {
+//   style: 'unit',
+//   unit: 'mile-per-hour',
 // };
-// const h = function () {
-//   const b = 777;
-//   console.log(b * 2);
-//   f = function () {
-//     console.log(b * 2);
-//   };
-// };
 
-// g();
-// f();
+// console.log('In:', new Intl.NumberFormat('en-IN', options).format(num));
+// console.log('US:', new Intl.NumberFormat('en-US', options).format(num));
+// console.log('Browser:', new Intl.NumberFormat(navigator.language).format(num));
 
-// // Re assigning f function
+//Settimeout
+// const ingredients = ['olives', 'Spinach'];
+// const pizzaTimer = setTimeout(
+//   (in1, in2) => console.log(`Here is your Pizza with ${in1} and ${in2}`),
+//   3000,
+//   ...ingredients
+// );
 
-// h();
-// f();
-// console.dir(f);
+// console.log('Waiting...');
 
-// const boardPassengers =  function(n, wait){
-//   const perGroup= n/3;
-//   setTimeout(function(){}, 1000)
-//   console.log(`Will start boarding in ${wait}`);
-// }
+// if (ingredients.includes('Spinach')) clearTimeout(pizzaTimer);
+
+// //Set Interval
+
+// setInterval(function () {
+//   const now = new Date();
+//   const hours = now.getHours();
+//   const minute = now.getMinutes();
+//   const seconds = now.getSeconds();
+//   console.log(`${hours}:${minute}:${seconds}`);
+// }, 1000);
