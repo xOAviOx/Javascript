5de710f6693eaf114b5a73283e7e4afa5378dd72
Avi Shukla <avishuklacode@gmail.com>
2025-01-09 22:28:07 +0530

    Update script.js

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -36,3 +36,12 @@ function goals (la, copa, ucl) {
   }
 }
 goals(5,10,2)
+function JadenString(str) {
+  const string = str.split(/ /g);
+
+  for (let i = 0; i < string.length; i++) {
+    string[i] = string[i][0].toUpperCase() + string[i].substr(1);
+  }
+  return string;
+}
+console.log(JadenString("How can mirrors be real if our eyes arent real"))
