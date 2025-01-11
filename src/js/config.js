0f1cf451e447aceb139a42f5a2d99b4cbf4a75ba
xOAviOx <avishuklacode@gmail.com>
2025-01-11 15:27:45 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b9425754-d71c-4f6d-b9d1-8a0890b58ae5';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,68 @@
+import View from './View';
+import icons from '../../img/icons.svg'; // Parcel
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      const gotoPage = +btn.dataset.goto;
+      // console.log(gotoPage);
+      handler(gotoPage);
+    });
+  }
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsperPage
+    );
+    // console.log(numPages);
+
+    //1. Page 1, and there are other pages
+    if (curPage === 1 && numPages > 1) {
+      return ` <button data-goto=${
+        curPage + 1
+      } class="btn--inline pagination__btn--next">
+              <span>Page ${curPage + 1}</span>
+                <svg class="search__icon">
+              <use href=${icons}#icon-arrow-right></use>
+            </svg>`;
+    }
+
+    //Last page
+    if (curPage === numPages && numPages > 1) {
+      return `<button data-goto=${
+        curPage - 1
+      } class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href=${icons}#icon-arrow-left></use>
+            </svg> 
+            <span>Page ${curPage - 1}</span>
+          </button>`;
+    }
+    //Other page
+    if (curPage < numPages) {
+      return `<button data-goto=${
+        curPage - 1
+      } class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href=${icons}#icon-arrow-left></use>
+            </svg> 
+            <span>Page ${curPage - 1}</span>
+          </button>
+          <button data-goto=${
+            curPage + 1
+          } class="btn--inline pagination__btn--next">
+              <span>Page ${curPage + 1}</span>
+                <svg class="search__icon">
+              <use href=${icons}#icon-arrow-right></use>
+            </svg>`;
+    }
+    //2. Page 1, and there are other pages
+    return '';
+  }
+}
+export default new PaginationView();
